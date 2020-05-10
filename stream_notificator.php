17604caa581d87bef59cc72673e6faf633ba8c63
hamscratch <pgalgs@gmail.com>
2020-05-10 15:33:25 -0400

    arg handling?

--- stream_notificator.php
+++ stream_notificator.php
@@ -1,10 +1,17 @@
 <?php
 
-CONST PETE_ID = '122085265';
-CONST MIKE_ID = '58761711';
+/* set up cron to run this with an argument of a USER_ID
+
+IDs:
+Pete = 122085265
+Mike = 58761711
+
+*/ 
+
+$user_id = $argv1
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-	$challenge = $_GET['hub.challenge'];
+	$challenge = $_GET["hub.challenge"];
 	http_response_code(200);
 	echo $challenge;
 	exit();
@@ -14,13 +21,13 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 // must be renewed. 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-	$url = 'https://api.twitch.tv/helix/webhooks/hub';
+	$url = "https://api.twitch.tv/helix/webhooks/hub";
 
 	$data = [
-		'hub.callback' => 'http://34.71.198.211/discord_commenter.php',
-		'hub.mode' => 'subscribe',
-		'hub.topic' => 'https://api.twitch.tv/helix/streams?user_id=122085265',
-		'hub.lease_seconds' => '864000',
+		"hub.callback" => "http://34.71.198.211/discord_commenter.php",
+		"hub.mode" => "subscribe",
+		"hub.topic" => "https://api.twitch.tv/helix/streams?user_id={$user_id}",
+		"hub.lease_seconds" => "864000",
 		];
 
 	$payload = json_encode($data);
