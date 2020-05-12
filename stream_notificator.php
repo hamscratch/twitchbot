a6b7970733bf26eb20ec1ca329d0773d65c13ac6
hamscratch <pgalgs@gmail.com>
2020-05-11 21:52:59 -0400

    added tokenValidator function

--- stream_notificator.php
+++ stream_notificator.php
@@ -3,43 +3,53 @@
 require __DIR__ . '/' . 'Loader.php';
 
 if (php_sapi_name() ==="cli") {
-    if (sizeof($argv > 1)) {
+    if (isset($argv[1])) {
         $twitch_user_id = $argv[1];
     } else {
         $twitch_user_id = Secrets::TWITCH_USER_ID;
     }
 
     $twitch_auth_token = Secrets::TWITCH_AUTH_TOKEN;
-    $host_url = Secrets::HOST_URL;
+    $is_valid_token = tokenValidator($twitch_auth_token);
 
-    $hub_url = "https://api.twitch.tv/helix/webhooks/hub";
-    $headers = array("Authorization: Bearer {$twitch_auth_token}", "Content-Type: application/json");
+    if ($is_valid_token == TRUE) {
 
-    // 864000 seconds = 10 days
-    $data = [
-        "hub.callback" => "{$host_url}/stream_notificator.php",
-        "hub.mode" => "subscribe",
-        "hub.topic" => "https://api.twitch.tv/helix/streams?user_id={$twitch_user_id}",
-        "hub.lease_seconds" => "864000",
-        ];
+        $twitch_auth_token = Secrets::TWITCH_AUTH_TOKEN;
+        $host_url = Secrets::HOST_URL;
+        $hub_url = "https://api.twitch.tv/helix/webhooks/hub";
+        $headers = array("Authorization: Bearer {$twitch_auth_token}", "Content-Type: application/json");
 
-    $payload = json_encode($data);
+        // 864000 seconds = 10 days
+        $data = [
+            "hub.callback" => "{$host_url}/stream_notificator.php",
+            "hub.mode" => "subscribe",
+            "hub.topic" => "https://api.twitch.tv/helix/streams?user_id={$twitch_user_id}",
+            "hub.lease_seconds" => "864000",
+            ];
 
-    $ch = curl_init();
+        $payload = json_encode($data);
 
-    curl_setopt($ch, CURLOPT_URL, $hub_url);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        $ch = curl_init();
 
-    $result = curl_exec($ch);
+        curl_setopt($ch, CURLOPT_URL, $hub_url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
 
-    if ($result) {
-        var_dump($result);
-        return json_decode($result, true);
+        $result = curl_exec($ch);
+
+        if ($result) {
+            var_dump($result);
+            return json_decode($result, true);
+        } else {
+            return false;
+        }
     } else {
-        return false;
+        // i should log this failure and report it
+        $error_comment = "HALT, NONE SHALL PASS WITHOUT A VALID TOKEN...WHICH YOU DO NOT POSESS.";
+        echo $error_comment;
+        exit();
     }
 } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
     return verifyHubChallenge();
@@ -56,6 +66,26 @@ function verifyHubChallenge() {
     http_response_code(200);
     echo $challenge;
     exit();
+}
+
+function tokenValidator($token) {
+    $validation_url = 'https://id.twitch.tv/oauth2/validate';
+    $headers = "Authorization: OAuth {$token}";
+
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $validation_url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_GET, true);
+
+    $result = curl_exec($ch);
+
+    if ($result) {
+        return json_decode($result, true); 
+    } else {
+        return false;
+    }
 }
 
 /*
