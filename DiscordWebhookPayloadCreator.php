313005986a9c98ccbd975932821cc92c6eb335f7
hamscratch <pgalgs@gmail.com>
2020-05-25 01:30:49 -0400

    this will break shit

--- DiscordWebhookPayloadCreator.php
+++ DiscordWebhookPayloadCreator.php
@@ -1,63 +1,38 @@
 <?php
 
-/* EXAMPLE
-
-{
-  "username": "Webhook",
-  "avatar_url": "https://i.imgur.com/4M34hi2.png",
-  "content": "Text message. Up to 2000 characters.",
-  "embeds": [
-    {
-      "author": {
-        "name": "Birdie♫",
-        "url": "https://www.reddit.com/r/cats/",
-        "icon_url": "https://i.imgur.com/R66g1Pe.jpg"
-      },
-      "title": "Title",
-      "url": "https://google.com/",
-      "description": "Text message. You can use Markdown here. *Italic* **bold** __underline__ ~~strikeout~~ [hyperlink](https://google.com) `code`",
-      "color": 15258703,
-      "fields": [
-        {
-          "name": "Text",
-          "value": "More text",
-          "inline": true
-        },
-        {
-          "name": "Even more text",
-          "value": "Yup",
-          "inline": true
-        },
-        {
-          "name": "Use `\"inline\": true` parameter, if you want to display fields in the same line.",
-          "value": "okay..."
-        },
-        {
-          "name": "Thanks!",
-          "value": "You're welcome :wink:"
-        }
-      ],
-      "thumbnail": {
-        "url": "https://upload.wikimedia.org/wikipedia/commons/3/38/4-Nature-Wallpapers-2014-1_ukaavUI.jpg"
-      },
-      "image": {
-        "url": "https://upload.wikimedia.org/wikipedia/commons/5/5a/A_picture_from_China_every_day_108.jpg"
-      },
-      "footer": {
-        "text": "Woah! So cool! :smirk:",
-        "icon_url": "https://i.imgur.com/fKL31aD.jpg"
-      }
-    }
-  ]
-}
-
-*/
-
 /**
  * 
  */
 class DiscordWebhookPayloadCreator {
-	
+	const GIFS = [
+		'https://media.giphy.com/media/5bvKQc6PLnqvhG0Bxj/giphy.gif', // nic cage
+		'https://media.giphy.com/media/RwLDkna2fN3fG/giphy.gif', // homer and bart on couch
+		'https://giphy.com/gifs/cat-national-soup-WJK2SABYwvEvm/giphy.gif', // cat wat
+		'https://media.giphy.com/media/l41YxszG6LI9jx69O/giphy.gif', // welcome thrillho
+		'https://media.giphy.com/media/127LCkdUYpgSgU/giphy.gif', // milhouse bonestorm
+		'https://media.giphy.com/media/Q7hGxSxIalZyo/giphy.gif', // coyote moon
+		'https://media.giphy.com/media/citBl9yPwnUOs/giphy.gif', // where's the any key?
+		'https://media.giphy.com/media/3o6Mb4d6BuhssF2OE8/giphy.gif', // if there's a better use for the internet
+		'https://media.giphy.com/media/OMK7LRBedcnhm/giphy.gif', // nerdddd
+		'https://media.giphy.com/media/xT5LMVErntn8tBWMbS/giphy.gif', // moe's family feed bag
+		'https://media.giphy.com/media/3oKIPwoeGErMmaI43S/giphy.gif', // mascot running from explosions
+		'https://media.giphy.com/media/l0MYGb1LuZ3n7dRnO/giphy.gif', // die hard welcome
+		'https://media.giphy.com/media/fqVQbYttUbZMQbHkQJ/giphy.gif', // the nightman cometh
+		'https://media.giphy.com/media/sT4S8nuzENtAY/giphy.gif', // assemble your crew
+		'https://media.giphy.com/media/B4jfJqiIxvU08/giphy.gif', // BTAS
+		'https://media.giphy.com/media/mxDZecDOOsWCA/giphy.gif', // so it begins
+		'https://media.giphy.com/media/l0HlPtbGpcnqa0fja/giphy.gif', // randy streaming
+		'https://media.giphy.com/media/l2Sqg1iEWObH3oz2E/giphy.gif', // power streamer
+		'https://media.giphy.com/media/l2Je733DgHfXIWEve/giphy.gif', // alf pogs
+		'https://media.giphy.com/media/SFp9RLto964Ew/giphy.gif', // poochie
+		'https://media.giphy.com/media/DyM4NUSmuJV3G/giphy.gif', // mr sparkle
+	];
+
+	const SPONSORS = [
+		"This stream is brought to you by our currenct sponsor, Charms Blowpops. Did you know that during World War II, the U.S. Army began including Charms candies in combat rations as a supplemental energy form. That tradition has continued with a few interruptions. - [Source](https://en.wikipedia.org/wiki/Charms_Blow_Pops)",
+		"This stream is brought to you by our currenct sponsor, the classic fan favorite, none other than the flame broiled Rodeo Cheeseburger from Burger King. Our new Rodeo Burger features a savory flame-grilled beef patty topped with sweet and smoky BBQ sauce and crispy, golden onion rings served on a toasted, sesame seed bun.",
+	];
+
 	public $user_id;
 	public $user_name;
 	public $game_title;
@@ -73,13 +48,14 @@ class DiscordWebhookPayloadCreator {
 	}
 
 	public function formatPayload() {
-		$gif = $this->gif_picker->pickGif();
+		$gif = $this->pickGif();
+		$sponsor_text = $this->pickSponsor();
 
 		$formatted_payload = 
 			["embeds" => [
 				[
 				"title" => "{$this->user_name} is now streaming!",
-				"description" => "This stream is brought to you by our currenct sponsor, Charms Blowpops. Did you know that during World War II, the U.S. Army began including Charms candies in combat rations as a supplemental energy form. That tradition has continued with a few interruptions. - [source](https://en.wikipedia.org/wiki/Charms_Blow_Pops)",
+				"description" => "{$sponsor_text}",
 				"color" => 0x8f00ff,
 				"fields" => [
 					[
@@ -88,8 +64,8 @@ class DiscordWebhookPayloadCreator {
 						"inline" => true
 					],
 					[
-						"name" => "Sponsor",
-						"value" => "<:blowpop:713971667604471821>",
+						"name" => "Title",
+						"value" => "{$this->stream_title}",
 						"inline" => true					
 					],
 					[
@@ -109,6 +85,18 @@ class DiscordWebhookPayloadCreator {
 	];
 
 	return $formatted_payload;
+	}
+
+	public function pickGif() : string {
+		$gif = array_rand(array_flip(self::GIFS), 1);
+
+		return $gif;
+	}
+
+	public function pickSponsor() : string {
+		$sponsor = array_rand(array_flip(self::SPONSORS), 1);
+
+		return $sponsor;
 	}
 
 }
