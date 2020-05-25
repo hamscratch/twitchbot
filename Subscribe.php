a68b513d09b47563e495718ffd6f81a2f21f7082
hamscratch <pgalgs@gmail.com>
2020-05-25 17:49:40 -0400

    sigh...

--- Subscribe.php
+++ Subscribe.php
@@ -14,7 +14,7 @@ $valid_user_ids = Secrets::VALID_USER_IDS;
 if (count($argv) > 1) {
 	if (in_array($argv[1], $valid_user_ids)) {
 		$message = "This is what argv[2] is... {$argv[2]}";
-		$log_namespace->log_error($message, $log_namespace);
+		$logger->log_error($message, $log_namespace);
 		$twitch_user_id = $argv[1];
 		if ($argv[2] === true) {
 			$twitch_stream->subscribeToUser($twitch_user_id, true);
