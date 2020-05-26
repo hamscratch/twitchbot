5b81e355ad9049db2bb4f3bef1d30f714cf620f1
hamscratch <pgalgs@gmail.com>
2020-05-25 20:03:42 -0400

    testing better logging

--- Logger.php
+++ Logger.php
@@ -18,10 +18,33 @@ class Logger {
 		$this->log($message, $path);
 	}
 
-	private function log(string $message, string $path) {
+	public function buildFailureLog(string $class, $call_name, $response = null) : string {
+		$empty_string_response = '';
+
+		$log_data = [
+			'class' => $class,
+			'failed_call' => $call_name,
+			'timestamp' => microtime(true)
+		];
+
+		$log_data['response'] = $response ?? $empty_string_response;
+
+		$failure_info = print_r($log_data, true);
+
+		return $failure_info;
+
+	}
+
+	private function log(string $message, string $path, string $failure_info = null) {
 		$log_base = '/var/log/apache2/';
 		$log_path = $log_base . $path;
 
-		error_log($message, 3, $log_path);
+		if ($failure_info) {
+			$log_message = $failure_info . $message . "\n";
+		} else {
+			$log_message = $message . "\n";
+		}	
+
+		error_log($log_message, 3, $log_path);
 	}
 }
--- Subscribe.php
+++ Subscribe.php
@@ -1,11 +1,21 @@
 <?php
 
 // This is a COMMAND LINE run script. You must provide a valid user id as an argument
+// If you just provide the user id, it will subscribe. If you <true> as the second arg, you will
+// unsubscribe to the user. 
+//
+// *** EXAMPLES ***
+//
+// To Subscribe to user 12345 stream: php Subscribe.php 12345
+// To Unsubscribe to user 12345 stream: php Subscribe.php 12345 true
+
 
 require __DIR__ . '/' . 'Loader.php';
 
 $twitch_stream = new TwitchStream();
 $logger = new Logger();
+$scipt_name = 'Subscribe.php';
+
 $log_namespace = 'subscribe';
 
 // array of valid user ids
@@ -15,8 +25,6 @@ if (count($argv) > 1) {
 	if (in_array($argv[1], $valid_user_ids)) {
 		$twitch_user_id = $argv[1];
 		if ($argv[2] == 'true') {
-			$message = "Sent {$argv[2]} to subscibeToUser";
-			$logger->log_error($message, $log_namespace);
 			$twitch_stream->subscribeToUser($twitch_user_id, true);
 		} else {
 	    	$twitch_stream->subscribeToUser($twitch_user_id);
@@ -26,8 +34,10 @@ if (count($argv) > 1) {
 		$logger->log_error($message, $log_namespace);
 	}
 } else {
+	$failure_info = $logger->buildFailureLog($scipt_name, 'subscribe');
+
 	$message = "Must provide a valid user id as script argument.";
-	$logger->log_error($message, $log_namespace);
+	$logger->log_error($message, $log_namespace, $failure_info);
 }
 
 
