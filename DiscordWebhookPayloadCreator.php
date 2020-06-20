dac34f8d33c19ed95e807c748294d09daeb4ca60
hamscratch <pgalgs@gmail.com>
2020-06-20 13:56:27 -0400

    new sponsor and gifs

--- DiscordWebhookPayloadCreator.php
+++ DiscordWebhookPayloadCreator.php
@@ -33,12 +33,31 @@ class DiscordWebhookPayloadCreator {
 		'https://media.giphy.com/media/l2Je6m6JQhZ8eByJq/giphy.gif', // pray for mojo
 		'https://media.giphy.com/media/xT5LMwerqFetjEnHji/giphy.gif', // use your nails
 		'https://media.giphy.com/media/l2JehbpbyVfTS8zZK/giphy.gif', // white suit
+		'https://media.giphy.com/media/xT5LMBfaEF1nnkwmoU/giphy.gif', // homer boxering
+		'https://media.giphy.com/media/3orieXwm24rqiQGsRa/giphy.gif', // mcbain let's get silly
+		'https://media.giphy.com/media/Na2i9xObnOz3W/giphy.gif', // mac and charlie what up?
+		'https://media.giphy.com/media/uTjpIPVMaYlBS/giphy.gif', // deadpool tweet
+		'https://media.giphy.com/media/kmdSKqlSHQJMs/giphy.gif', // spidey sense
+		'https://media.giphy.com/media/oW4csEbiMzVjq/giphy.gif', // tobey dancing
+		'https://media.giphy.com/media/WEopdZPzAlXzy/giphy.gif', // but they were cones
+		'https://media.giphy.com/media/ANbD1CCdA3iI8/giphy.gif', // serious fry
+		'https://media.giphy.com/media/L17Rt82QrScQU/giphy.gif', // tired slurm
+		'https://media.giphy.com/media/3o7abA4a0QCXtSxGN2/giphy.gif', // good news everyone!
+		'https://media.giphy.com/media/RmfzOLuCJTApa/giphy.gif', // jerry smithing
+		'https://media.giphy.com/media/brqkBQV1qAFrO/giphy.gif', // here i go killing again
+		'https://media.giphy.com/media/DgLsbUL7SG3kI/giphy.gif', // meeseeks cannnn do!
+		'https://media.giphy.com/media/qPVzemjFi150Q/giphy.gif', // my man!
+		'https://media.giphy.com/media/h0Xez8ow1UOVq/giphy.gif', // jim carrey riddler
+		'https://media.giphy.com/media/3o7TKtIZdurmU8Igvu/giphy.gif', // mr poopy b hole
+		'https://media.giphy.com/media/vMnuZGHJfFSTe/giphy.gif', // wayne's world excellent
+		'https://media.giphy.com/media/Nx0rz3jtxtEre/giphy.gif', // hello there kenobi
 	];
 
 	const SPONSORS = [
 		"This stream is brought to you by our currenct sponsor, Charms Blowpops. Did you know that during World War II, the U.S. Army began including Charms candies in combat rations as a supplemental energy form. That tradition has continued with a few interruptions. - [Source](https://en.wikipedia.org/wiki/Charms_Blow_Pops)",
 		"This stream is brought to you by our currenct sponsor, the classic fan favorite, none other than the flame broiled Rodeo Cheeseburger from Burger King. Our new Rodeo Burger features a savory flame-grilled beef patty topped with sweet and smoky BBQ sauce and crispy, golden onion rings served on a toasted, sesame seed bun.",
 		"This stream is brought to you by our current sponsor, [Nickelodeon's Gak](https://www.youtube.com/watch?v=_iDALjY4QnY). Did you know name 'gak' had previously been used by Marc Summers and the Double Dare crew to refer to slime used on the show. The name had originated as a street term for heroin. This was a source of amusement behind the scenes of Double Dare when Nickelodeon used the term for the toy Gak, unaware of its origin in drug culture. - [Source](https://en.wikipedia.org/wiki/Nickelodeon_Toys#Gak)",
+		"This stream is brought to you by our current sponsor, [Fight Milk](https://www.youtube.com/watch?v=BEMlvjJ9uxo). The first alcoholic, dairy-based protein drink for bodyguards by bodyguards! With their patented blend of crow egg, milk, and vodka, Fight Milk will give you your daily dose of Crowtein needed to be the best bodyguard you can be. Drink it to fight like a crow!",
 	];
 
 	public $user_id;
